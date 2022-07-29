5a90d049c5f29dcb2f179ef3efdbc1919298d00f
Mingo <whmzsu@gmail.com>
2022-07-29 16:39:43 +0800

    update json dump parameter

--- json_test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-with open("D:\Study\Python\json\FC.JSON", "r") as f:
-    game = json.load(f)
-
-content = json.dumps(game, indent=4)
-with open("D:\Study\Python\json\dump.json", "w") as f:
-    f.write(content)
--- retroarch_pl_generator.py
+++ retroarch_pl_generator.py
@@ -24,6 +24,6 @@ for root, dirs, files in os.walk(romspathdir):
             list1.append({"path": rom_path,
                           "label": os.path.splitext(file)[0], "core_path": "DETECT", "core_name": "DETECT", "crc32": "DETECT", "db_name": os.path.basename(root)+'.ipl'})
         dict1 = {"items": list1}
-        content = json.dumps(dict1, indent=4)
+        content = json.dumps(dict1, indent=4, ensure_ascii=False)
         with open(os.path.join(iplpathdir, os.path.basename(root))+".ipl", "w") as f:
             f.write(content)
